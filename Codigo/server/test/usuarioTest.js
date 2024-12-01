1821db2576047e1e9c9dbda2936303c267082df8
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-12-01 17:14:39 -0300

    Corrigindo teste de usuario

--- Codigo/server/test/usuarioTest.js
+++ Codigo/server/test/usuarioTest.js
@@ -5,17 +5,21 @@ import app from '../index.js';
 describe('POST /cadastro', () => {
 	it('deve retornar erro quando usuário não receber permissão', (done) => {
 		const newUser = {
-			nome: "O JavaScript",
-			email: "js@gmail.com",
-			senha: "javaScript>>>Java"
+			nome: "Um nome qualquer",
+			email: "umemail@qualquer.com",
+			senha: "umasenhaqualquer"
 		}
 
 		request(app)
 			.post('/cadastro')
 			.send(newUser)
 			.end((err, res) => {
-				if (err) return done(err);
-				assert.strictEqual(res.body.tipo, "Erro de cadastro");
+				if(err) {
+					assert.equal(res.body.tipo, "Erro de cadastro")
+					return done(err);
+				}
+
+				assert.equal(res.body.tipo, "Cadastro realizado com sucesso")
 				done();
 			});
 	})
@@ -32,8 +36,11 @@ describe('POST /login', () => {
 			.post('/login')
 			.send(newLogin)
 			.end((err, res) => {
-				if (err) return done(err);
-				assert.strictEqual(res.body.tipo, "Usuário não encontrado");
+				if (err){;
+					assert.strictEqual(res.body.tipo, "Usuário não encontrado");
+					return done(err);
+				}
+				assert.equal(res.body.tipo, "Login realizado com sucesso");
 				done();
 			});
 	})
