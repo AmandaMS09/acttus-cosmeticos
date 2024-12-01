9d899095ec857f19d73996067483ac6dff58161f
Pedro Ramos Vidigal <104322803+PedroRamos111@users.noreply.github.com>
2024-12-01 20:58:07 -0300

    Update cadastro.steps.js

--- Codigo/features/step_definitions/cadastro.steps.js
+++ Codigo/features/step_definitions/cadastro.steps.js
@@ -1,7 +1,7 @@
 const API_URL='https://acttus-cosmeticos.vercel.app'
-const { Given, When, Then } = require('@cucumber/cucumber');
-const fetch = require('node-fetch');
-const assert = require('assert');
+import { Given, When, Then } from '@cucumber/cucumber';
+import fetch from 'node-fetch';
+import assert from 'assert';
 
 let response = {};
 let usuario = { nome: '', email: '', senha: '' };
